3b6e30305e2b516230142a6b7eab7d38de2dd1fc
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-10-25 22:32:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.Write("Введите x:");
+    int x = int.Parse(Console.ReadLine());
+    Console.Write("Введите n:");
+    int n = int.Parse(Console.ReadLine());
+    double P = 1;
+    double S = 0;
+    for (int k = 1; k <= n; k++)
+    {
+        if (k != 2) S += Math.Pow(-3, 3 * k + 1) / (k - 2) * Math.Pow(x, 3 * k + 1);
+        for (int m = 1; m <= k + n; m++)
+        {
+            if (m != 11) P *= m * m * m - 8 / m - 3;
+        }
+    }
+   
+    Console.WriteLine($"S={P * S:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
